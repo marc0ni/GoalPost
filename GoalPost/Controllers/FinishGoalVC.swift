c9ced986d1608dbe027bbbd3e6701e6140fc647a
Mark Lindamood <mark.lindamood@yahoo.com>
2017-10-26 03:10:26 -0700

    FinishGoalVC view controller and UI added

---
+++ GoalPost/Controllers/FinishGoalVC.swift
@@ -0,0 +1,64 @@
+//
+//  FinishGoalVC.swift
+//  GoalPost
+//
+//  Created by Mark Lindamood on 10/26/17.
+//  Copyright © 2017 udemy. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class FinishGoalVC: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var createGoalBtn: UIButton!
+    @IBOutlet weak var pointsTextField: UITextField!
+    
+    var goalDescription: String!
+    var goalType: GoalType!
+    
+    func initData(description: String, type: GoalType) {
+        self.goalDescription = description
+        self.goalType = type
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createGoalBtn.bindToKeyboard()
+        pointsTextField.delegate = self
+    }
+    
+    func save(completion: (_ finished: Bool) -> ()) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let goal = Goal(context: managedContext)
+        
+        goal.goalDescription = goalDescription
+        goal.goalTerm = goalType.rawValue
+        goal.goalCompletionValue = Int32(pointsTextField.text!)!
+        goal.goalProgress = Int32(0)
+        
+        do {
+            try managedContext.save()
+            print("Successfully saved data.")
+            completion(true)
+        } catch {
+            debugPrint("Could not save: \(error.localizedDescription)")
+            completion(false)
+        }
+    }
+    
+    @IBAction func createGoalBtnPressed(_ sender: Any) {
+        if pointsTextField.text != "" {
+            self.save { (complete) in
+                if complete {
+                    dismiss(animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismissDetail()
+    }
+    
+    
+}
