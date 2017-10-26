b1a09c99cb117a8649af7e0e5fff41de6516f5d1
Mark Lindamood <mark.lindamood@yahoo.com>
2017-10-26 03:05:41 -0700

    Added UIButton, UIViewController and UIView extensions

---
+++ GoalPost/Extensions/UIButtonExt.swift
@@ -0,0 +1,19 @@
+//
+//  UIButtonExt.swift
+//  GoalPost
+//
+//  Created by Mark Lindamood on 10/24/17.
+//  Copyright © 2017 udemy. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton{
+    func setSelectedColor(){
+        self.backgroundColor = #colorLiteral(red: 0.4922404289, green: 0.7722371817, blue: 0.4631441236, alpha: 1)
+    }
+    
+    func setDeselectedColor(){
+        self.backgroundColor = #colorLiteral(red: 0.7464011312, green: 0.8857318759, blue: 0.7402122021, alpha: 1)
+    }
+}
