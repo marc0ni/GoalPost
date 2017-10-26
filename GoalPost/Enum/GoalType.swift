1b622c693b6ac58ab2fa2c9cc8831f2216e8bc80
Mark Lindamood <mark.lindamood@yahoo.com>
2017-10-26 02:54:23 -0700

    Added GoalType enum

---
+++ GoalPost/Enum/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  GoalPost
+//
+//  Created by Mark Lindamood on 10/26/17.
+//  Copyright © 2017 udemy. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
